13378d7f2f5ccec62fb5029ddaf39491dd03ef13
Robin <rowinski@01digitalesdesign.de>
2014-04-25 10:46:21 +0200

    Implemented navigation w/ active passive states

--- BeaconScanner/Sources/com/dd/beaconscanner/components/NavigationBar.java
+++ BeaconScanner/Sources/com/dd/beaconscanner/components/NavigationBar.java
@@ -3,7 +3,33 @@ package com.dd.beaconscanner.components;
 import com.webobjects.appserver.WOContext;
 
 public class NavigationBar extends BaseComponent {
-    public NavigationBar(WOContext context) {
+    
+	public NavigationBar(WOContext context) {
         super(context);
     }
+	
+	public boolean isSelectedPersons() {
+		return (context().page() instanceof PersonOverview);
+	}
+	
+	public boolean isSelectedRadar() {
+		return (context().page() instanceof RadarOverview);
+	}
+	
+	public boolean isSelectedAssets() {
+		return (context().page() instanceof AssetOverview);
+	}
+	
+	public String cssClassPersons() {
+		return isSelectedPersons() ? "active" : null;
+	}
+	
+	public String cssClassRadar() {
+		return isSelectedRadar() ? "active" : null;
+	}
+	
+	public String cssClassAssets() {
+		return isSelectedAssets() ? "active" : null;
+	}
+	
 }
